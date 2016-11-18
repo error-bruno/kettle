43c9bda5fa5bfa1e4db04a483c8d0ebf2fc5a3bb
Bruno <archangel43@gmail.com>
2016-11-17 19:02:57 -0500

    Add helmet

--- src/browser/App/App.react.js
+++ src/browser/App/App.react.js
@@ -1,7 +1,18 @@
 import React, { PropTypes } from 'react';
+import Helmet from 'react-helmet';
 
 const App = ({ name, initializeStore, isInitialized }) => (
   <div>
+    <Helmet
+      titleTemplate="%s - Kettle"
+      defaultTitle="Rivers Agile Kettle Pot"
+      meta={[
+        {
+          name: 'description',
+          content: 'A seed project for creating beautiful things.'
+        }
+      ]}
+    />
     Hello, {name}!
     <button onClick={() => initializeStore()}>Init</button>
     <p>{isInitialized ? 'App is inited' : 'App is not inited'}</p>
